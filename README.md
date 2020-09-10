94283947fe1e4cd5c710323a913836cba8b3a94f
Oscar Ledezma <ooledezma@gmail.com>
2020-09-09 19:29:21 -0700

    Renders html file

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# Team-Generator
+### **Team Generator**
+
+## Description
+
+Team Generator is a command line application that builds a software engineering team.The application will prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns.
+
+## Table of contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [GitHub Info](#Questions)
+
+## Installation
+
+To run this application first you nust run npm install after just type node app.js in the command line.
+
+## Usage
+
+This application assist the user in creating a software engineering team, when the user has completed building the team, the application will create an HTML file with the rostr. The user can use this to have quick access to emails and GitHub profiles.
+
+## Screenshots
+
+How it works:
+
+![](Assets/capture.gif)
+
+Example of HTML Generated:
+
+![](Assets/Capture.PNG)
+
+## License
+
+![License](https://img.shields.io/badge/LICENSE-MIT-GREEN)
+
+## Contributors
+
+No contributing guidelines
+
+## Test
+
+used Jest to test this application npm run test
+
+## Questions
+
+For questions contact ooledezma@gmail.com.
+GitHub URL: https://api.github.com/users/ooledezma
--- app.js
+++ app.js
@@ -11,10 +11,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const { type } = require("os");
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employees = [];
 
-console.log("Please build your team");
 
 var questions = [
   {
@@ -98,18 +96,20 @@ var choiceQuestion = [
   },
 ];
 
-function manager() {
-  inquirer.prompt(questions).then((answers) => {
-    
-    var manager = new Manager (answers.officenumber)
-    console.log(manager);
-    choices();
-  });
-}
+console.log("Please build your team");
+inquirer.prompt(questions).then((answers) => {
+  const manager = new Manager(
+    answers.name,
+    answers.id,
+    answers.email,
+    answers.officeNumber
+  );
+  employees.push(manager);
+  choices();
+});
 
 function choices() {
   inquirer.prompt(choiceQuestion).then((answers) => {
-    console.log(answers);
     switch (answers.type) {
       case "Engineer":
         engineer();
@@ -117,44 +117,44 @@ function choices() {
       case "Intern":
         intern();
         break;
-      //   case "I don't want to add any more team members":
-      //     break;
+      case "I don't want to add any more team members":
+        renderhtml();
+        break;
     }
   });
 }
 
 function engineer() {
   inquirer.prompt(engineerQuestions).then((answers) => {
-    console.log(answers);
+    const engineer = new Engineer(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.github
+    );
+    employees.push(engineer);
     choices();
   });
 }
 
 function intern() {
   inquirer.prompt(internQuestions).then((answers) => {
-    console.log(answers);
+    const intern = new Intern(
+      answers.name,
+      answers.id,
+      answers.email,
+      answers.school
+    );
+    employees.push(intern);
     choices();
   });
 }
 
-manager();
+function renderhtml() {
+  console.log(employees);
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+  fs.writeFile(outputPath, render(employees), (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+}
